2b507d1dabe26d65d02db92e94f35377cab5c510
huuhoa0322 <huuhoa040322@gmail.com>
2026-01-21 23:39:47 +0700

    scaffold db and change app settings

---
+++ HE186716_DoHuuHoa_SE1884-NET_A01_BE/Models/SystemAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HE186716_DoHuuHoa_SE1884_NET_A01_BE.Models;
+
+public partial class SystemAccount
+{
+    public short AccountId { get; set; }
+
+    public string? AccountName { get; set; }
+
+    public string? AccountEmail { get; set; }
+
+    public int? AccountRole { get; set; }
+
+    public string? AccountPassword { get; set; }
+
+    public virtual ICollection<NewsArticle> NewsArticles { get; set; } = new List<NewsArticle>();
+}
---
+++ HE186716_DoHuuHoa_SE1884-NET_A01_BE/Models/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HE186716_DoHuuHoa_SE1884_NET_A01_BE.Models;
+
+public partial class Tag
+{
+    public int TagId { get; set; }
+
+    public string? TagName { get; set; }
+
+    public string? Note { get; set; }
+
+    public virtual ICollection<NewsArticle> NewsArticles { get; set; } = new List<NewsArticle>();
+}
